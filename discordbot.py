b9f9ad942df234b1f8f665994053dab2f66f9d7d
tshi-github <132886714+tshi-github@users.noreply.github.com>
2024-07-23 11:35:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,26 +1,39 @@
+# 土日月は木曜日に絶対取得
+# 水曜日は月火曜
+
 import discord
-import traceback
-from discord.ext import commands
-from os import getenv
+from discord.ext import commands, tasks
+import datetime
+import os
+
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
-
-bot = commands.Bot(command_prefix='/', intents=intents)
-
+bot = commands.Bot(command_prefix='>', intents=intents)
 
 @bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    check_time.start()
 
+@tasks.loop(minutes=1)
+async def check_time():
+    dt_now = datetime.datetime.now()
+    if dt_now.hour == 11 and dt_now.minute == 35:
+        weekday_message = await checkweekday()
+        # CHANNEL_ID を実際のチャンネルIDに置き換えてください
+        channel = bot.get_channel('MTI1NDgyMDU1NzYwMjQyNjg5MA.Gcm0B_.HFpUJO07LQUr389ie9gym5J-5JO3oILuiR3SKY')
+        if channel:
+            await channel.send(weekday_message)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def checkweekday():
+    today = datetime.date.today()
+    if today.weekday() == 0:
+        return "水曜日の会議室の予約行け！！！"
+    elif today.weekday() == 3:
+        return "土日月の会議室の予約行け！！！"
+    elif today.weekday() in [1, 4]:
+        return "学生課で警備室用の紙を回収せよ！！！"
 
-
-token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
-
+bot.run('MTI1NDgyMDU1NzYwMjQyNjg5MA.Gcm0B_.HFpUJO07LQUr389ie9gym5J-5JO3oILuiR3SKY')
