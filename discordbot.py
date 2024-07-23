1d174564a25f9183929c2300abb1f9b129e8e0be
tshi-github <132886714+tshi-github@users.noreply.github.com>
2024-07-23 15:35:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,4 @@
 import discord
-import traceback
 from discord.ext import commands
 from os import getenv
 
@@ -8,18 +7,9 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
 
 token = getenv('TOKEN')
 bot.run(token)
